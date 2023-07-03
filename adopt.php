f01761fea5ccfe7efd386b080d218f66a6cfa80b
daniel Kabagambe <danielkabagambe@gmail.com>
2023-07-03 18:02:15 +0300

    adoption form

--- adopt.php
+++ adopt.php
@@ -2,18 +2,21 @@
 if(isset($_POST['submit'])){
     $to = 'info@dolphinfoundationvillage.org'; // Updated email address to receive the email
     $from = $_POST['email']; // This is the sender's Email address
-    $first_name = $_POST['firstname'];
-    $last_name = $_POST['lastname'];
+ 
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+    $age = $_POST['age'];
+    $newDate = date('Y-m-d');
+ 
+    $telephone = $_POST['telephone'];
+    $telephon = $_POST['telephon'];
+  
     $subject = 'Form submission';
-    $subject2 = 'Copy of your form submission';
-    $message = $first_name . ' ' . $last_name . ' wrote the following: ' .  $_POST['message'];
-    $message2 = 'Here is a copy of your message ' . $first_name . ': ' . $_POST['message'];
+    $message = $first_name . ' ' . $last_name . ' expressed interest on ' . $newDate . ' regarding fostering a child aged between ' . $_POST['adopt'] . ' months. ' . $last_name . ' is from ' . $_POST['address'] . ' and is ' . $_POST['age'] . ' years old. Their marital status is ' . $_POST['marital'] . '. Their email addresses are ' . $_POST['email'] . ' and ' . $_POST['email1'] . '. Their telephone numbers are ' . $telephone . ' and ' . $telephon . '. Their motivation to adopt is: ' . $_POST['subject1'] . '. They learned about adoption in Uganda from: ' . $_POST['subject2'] . '. Additional comments: ' . $_POST['subject3'];
 
     $headers = 'From:' . $from;
-    $headers2 = 'From:' . $to;
     mail($to, $subject, $message, $headers);
-    mail($from, $subject2, $message2, $headers2); // Sends a copy of the message to the sender
-    echo 'Mail Sent. Thank you, ' . $first_name . ', we will contact you shortly.';
+    echo 'Mail Sent. Thank you for showing interest in Dolphin, ' . $first_name . '. We will contact you shortly.';
     header('Location: index.html'); // Redirect to index.html
     exit();
 }
